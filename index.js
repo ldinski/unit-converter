c81f1dbd0a5d65f9a7e6b0c286a419e3c3238088
ldinski <95621292+ldinski@users.noreply.github.com>
2022-07-11 17:04:34 +0100

    styling + js
    finished the styling for the page
    
    finished the conversion calculations so when an input is taken, the output is shown based on that input and calculations done also

--- index.js
+++ index.js
@@ -3,3 +3,33 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+let inputEl = document.querySelector(".input-field");
+let convertBtn = document.querySelector(".convert-btn");
+let metreFeet = document.querySelector(".metreFeet");
+let litreGallon = document.querySelector(".litreGallon");
+let kgPounds = document.querySelector(".kgPounds");
+
+const metreToFeet = 3.281;
+const litreToGallon = 0.219;
+const kgToPound = 2.204;
+
+convertBtn.addEventListener("click", function () {
+  let baseValue = inputEl.value;
+
+  metreFeet.textContent = `${baseValue} metres = ${(
+    baseValue * metreToFeet
+  ).toFixed(3)} feet | ${baseValue} feet = ${(baseValue / metreToFeet).toFixed(
+    3
+  )} metres`;
+
+  litreGallon.textContent = `${baseValue} litres = ${(
+    baseValue * litreToGallon
+  ).toFixed(3)} gallons | ${baseValue} gallons = ${(
+    baseValue / litreToGallon
+  ).toFixed(3)} litres`;
+
+  kgPounds.textContent = `${baseValue} kg = ${(baseValue * kgToPound).toFixed(
+    3
+  )} pounds | ${baseValue} pounds = ${(baseValue / kgToPound).toFixed(3)} kg`;
+});
